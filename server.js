5d040bf18026fb893b40a8b79b760ec4ab058474
Andre <andrep@tlu.ee>
2017-10-24 14:39:49 +0300

    Ilm.ee route lisamine server.js-i

--- server.js
+++ server.js
@@ -9,10 +9,14 @@ var http = require('http');
 var server = http.createServer(app);
 var port = process.env.PORT || 8080;
 
+var ilm = require('./routes/ilm');
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use('/api/ilm', ilm);
 
 app.all('/*', function(req,res, next){
     res.sendFile('/public/index.html', {root:__dirname});
