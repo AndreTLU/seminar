fffc02d55ba2ea02dab47982d4effc19b016f1c6
Andre <andrep@tlu.ee>
2017-10-24 13:16:32 +0300

    Nodejs teekide lisamine server.js-i

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var request = require('request');
+var app = express();
+
 var http = require('http');
 var server = http.createServer();
 var port = process.env.PORT || 1337;
+
 server.listen(port, function(){
     console.log('Server started on port ' + port);
 })
