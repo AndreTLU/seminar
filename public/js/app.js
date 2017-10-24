31520a96711bf52bb7a50db7a62f60d2efa5677c
Andre <andrep@tlu.ee>
2017-10-24 14:32:11 +0300

    Angular HTML5 mode

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,7 @@ angular.module('seminaritoo', ['ui.router', 'ngResource'])
     .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
         function($stateProvider, $urlRouterProvider, $locationProvider){
             $urlRouterProvider.otherwise('/');
+
+            $locationProvider.html5Mode(true);
         }    
 ])
