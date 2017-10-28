b8faa8d853beef2e78bdc44a0b8b67a9f9701636
Andre <andrep@tlu.ee>
2017-10-28 10:36:43 +0300

    Service Workeri registreerimine Angulari poolel

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,22 @@ angular.module('seminaritoo', ['ui.router', 'ngResource'])
     .config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
         function($stateProvider, $urlRouterProvider, $locationProvider){
             $urlRouterProvider.otherwise('/');
-
+            registerServiceWorker();
             $locationProvider.html5Mode(true);
         }    
-])
+]);
+
+registerServiceWorker = function () {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function () {
+            navigator.serviceWorker.register('sw.js').then(function (registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+    
+}
