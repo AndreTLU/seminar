0df621064ab627e0ed094586cb2ce48321a12718
Andre <andrep@tlu.ee>
2017-10-28 10:40:09 +0300

    HomeController andmete pärimine

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,6 +1,25 @@
 angular.module('seminaritoo')
 .controller('HomeCtrl', ['$http', '$scope', '$rootScope',
     function($http, $scope, $rootScope){
-        
+        $scope.getData = function () {
+            if ('caches' in window) {
+                caches.match("/api/ilm/ee").then(function (response) {
+                    if (response) {
+                        response.json().then(function (json) {
+                            console.log('Uuendan vahemälust');
+                            $rootScope.requesPending = false;
+                            $scope.json = json;
+                            $scope.$apply();
+                        })
+                    }
+                })
+            }
+            $http.get('/api/ilm/ee').then(function (data) {
+                console.log(data);
+                console.log('Uuendan veebist');
+                $scope.json = data.data;
+            });
+        };
+        $scope.getData();
     }
 ]);
