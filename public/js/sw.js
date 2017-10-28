49094a2750c06bb64daa9fe4c562ea61c242f8d1
Andre <andrep@tlu.ee>
2017-10-28 10:33:01 +0300

    Service Worker fetch käsk

--- public/js/sw.js
+++ public/js/sw.js
@@ -21,4 +21,42 @@ self.addEventListener('install', function(event) {
           return cache.addAll(urlsToCache);
         })
     );
-  });
+});
+  self.addEventListener('activate', function(event) {
+    console.log('Activating Service Worker ' + cacheName);
+        event.waitUntil(
+            caches.keys().then(function(keyList) {
+                return Promise.all(keyList.map(function(key) {
+                    if (key != cacheName && key !== dataCacheName) {
+                        console.log('Service Worker - Removing old data');
+                        return caches.delete(key);
+                    }
+                }))
+            })
+    ); return self.clients.claim();
+    }
+);
+self.addEventListener('fetch',
+function (event) {
+    console.log('Service Worker - FETCH ', event.request.url);
+    var dataUrl = "https://seminaritoo.azurewebsites.net/api/ilm/ee";
+    if (event.request.url.indexOf(dataUrl) > -1) {
+        event.respondWith(
+            fetch(event.request)
+                .then(function (response) {
+                    return caches.open(dataCacheName).then(function (cache) {
+                        cache.put(event.request.url, response.clone());
+                        console.log('Service worker Fetched & cached data');
+                        return response;
+                    })
+                })
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request);
+            })
+        )
+    }
+}
+);
