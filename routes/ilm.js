781661f2ae67fa2c2f5872f2ea3cf6669020ce57
Andre <andrep@tlu.ee>
2017-11-04 11:00:41 +0200

    ilm.js uuendus

--- routes/ilm.js
+++ routes/ilm.js
@@ -5,7 +5,6 @@ var request = require('request');
 router.get('/ee/:city', function(req, res){
     var cityName = "";
     var url = "http://ilm.ee/sisu/2015/json_eesti_linnade_ilm.php3?l="+req.params.city;
-    //var extra_url = "https://ilm.ee/sisu/2015/json_reaal_linn.php3?linn=L*"+cityName+"*0";
     request({
         url: url,
         json: true
@@ -13,18 +12,14 @@ router.get('/ee/:city', function(req, res){
         function(err, response, data){
             if(!err && response.statusCode === 200){
                 var tempString = data.link;
-                cityName = tempString.slice(1,-1);
-
-                request({url: "https://ilm.ee/sisu/2015/json_reaal_linn.php3?linn=L*"+cityName+"*0",
+                cityName = data.link.slice(1,-1);
+                request({
+                url: "https://ilm.ee/sisu/2015/json_reaal_linn.php3?linn=L*"+cityName+"*0",
                 json: true},function(err, response, body){
                     if(!err && response.statusCode === 200){
-                        data.extra = body;
-                        console.log(data);
-                        res.status(200).json(data);
+                        data.extra = body; res.status(200).json(data);
                     }
                 })
-                
-                //res.status(200).json(body);
             }
         }
     ); 
