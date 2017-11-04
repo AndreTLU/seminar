c29d3f81fedfde4f695077499eb73d64a17fac70
Andre <andrep@tlu.ee>
2017-11-04 10:36:32 +0200

    Service Workeri failide asukoha uuendus

--- public/js/sw.js
+++ public/js/sw.js
@@ -1,52 +1,66 @@
 var cacheName = 'Seminaritoo-cache-v1';
 var dataCacheName = 'data-v1';
 var urlsToCache = [
-    '/',
-    '/public/index.html',
-    './routes/ilm.js',
-    './public/js/app.js',
-    './public/js/home/home.html',
-    './public/js/home/home.js',
-    './public/js/sw.js',
-    './server.js'
-    
+    '/js/home/home.js',
+    '/js/home/home.html',
+    '/js/app.js',
+    '/js/sw.js',
+    '/index.html',
+    '/'
 ];
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
+    // Perform install steps
+    console.log('Installing Service Worker!');
     event.waitUntil(
         caches.open(cacheName)
-        .then(function(cache) {
-          return cache.addAll(urlsToCache);
-        })
+            .then(function (cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
-  self.addEventListener('activate', function(event) {
-        event.waitUntil(
-            caches.keys().then(function(keyList) {
-                return Promise.all(keyList.map(function(key) {
-                    if (key != cacheName && key !== dataCacheName) {
-                        return caches.delete(key);
-                    }
-                }))
-            })
-    );
-    }
-);
-self.addEventListener('fetch',
-function (event) {
-    if (event.request.url.indexOf("/api/ilm/ee") > -1) {
-        event.respondWith(fetch(event.request)
-                .then(function (response) {
-                    caches.delete(dataCacheName);
-                    return caches.open(dataCacheName).then(function (cache) {
-                        cache.put(event.request.url, response.clone());
-                        return response;
-                    })}));
-    } else {
-        event.respondWith(
-            caches.match(event.request).then(function (response) {
-                return response || fetch(event.request);
-            })
-        )
-    }
+self.addEventListener('activate', function (event) {
+    console.log('Activating Service Worker ' + cacheName);
+    event.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key != cacheName && key !== dataCacheName) {
+                    console.log('Service Worker - Removing old data');
+                    return caches.delete(key);
+                }
+            }))
+        })
+    ); return self.clients.claim();
 }
 );
+self.addEventListener('fetch',
+    function (event) {
+        console.log('Service Worker - FETCH ', event.request.url);
+        if (event.request.url.indexOf("https://seminaritoo.azurewebsites.net/api/ilm/ee") > -1) {
+            event.respondWith(
+                fetch(event.request)
+                    .then(function (response) {
+                        caches.delete(dataCacheName);
+                        return caches.open(dataCacheName).then(function (cache) {
+                            cache.put(event.request.url, response.clone());
+                            return response;
+                        })
+                    })
+            );
+        } else {
+            event.respondWith(
+                caches.match(event.request).then(function (response) {
+                    return response || fetch(event.request);
+                })
+            )
+        }
+    }
+);
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+      caches.match(event.request)
+            .then(function(response){
+              return response ? response : fetch(event.request);
+            })
+    );
+  });
